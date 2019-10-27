8eeefbda3f69254ac61cf2d8024b9a1b7416baf8
Mohammed <Mohammed.ofisan@hotmail.com>
2019-10-27 23:17:29 +0300

    finish the homework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                {/* <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+        </p>  */}
+                <Movie title="The Fellowship of the Ring" hours="2" minutes="58"></Movie>
+                <Movie title="The Two Towers" hours="2" minutes="59"></Movie>
+                <Movie title="The Return of the King" hours="3" minutes="21"></Movie>
+            </div>
+        );
+    }
 }
 
 export default App;
